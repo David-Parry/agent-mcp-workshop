6913270b80de0628910e94f55b6a4f565fbc9852
davidparry <david.p@codium.ai>
2025-09-17 19:14:37 -0400

    Add clean-checkout script for branch switching

---
+++ clean-checkout.sh
@@ -0,0 +1,20 @@
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <branch>"
+    echo
+    echo "Examples:"
+    echo "  $0 complete"
+    echo "  $0 01-chapter"
+    echo "  $0 02-chapter"
+    echo "  $0 03-chapter"
+    echo "  $0 04-chapter"
+    echo "  $0 05-chapter"
+
+    exit 1
+
+fi
+
+BRANCH=$1
+
+git fetch origin
+git checkout -f -B $BRANCH origin/$BRANCH
+git clean -fdx
